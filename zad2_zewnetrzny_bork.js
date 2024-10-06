2029b49885564f270987abbf37271f9276539cd4
MarcelBorkowski <144600511+MarcelBorkowski@users.noreply.github.com>
2024-10-06 19:04:29 +0200

    Update zad2_zewnetrzny_bork.js

--- zad2_zewnetrzny_bork.js
+++ zad2_zewnetrzny_bork.js
@@ -1,3 +1,3 @@
-document.write ("<b>" "ostatnia modyfikacja strony".fontcolor("adua").bold().fontsize(6)+"</b>" "<br>"); 
+document.write ("<b>"+"ostatnia modyfikacja strony".fontcolor("adua").bold().fontsize(6)+"</b>"+"<br>"); 
 document.write (document.lastModified);
  
